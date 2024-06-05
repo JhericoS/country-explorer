8e9602ee0e0452c47ec671991d687158be1b5cd6
Jherico <jhericof.sv.189@gmail.com>
2024-06-05 13:27:04 -0500

    add images and fliters

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CountryCard from "@/components/country-card";
 import SearchBar from "@/components/search-bar";
 import { gql, useSuspenseQuery } from "@apollo/client";
@@ -7,6 +7,10 @@ import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
 
 const query = gql`
   query {
+    continents {
+      name
+      code
+    }
     countries {
       code
       continent {
@@ -39,23 +43,93 @@ function Home() {
 
   const { data } = useSuspenseQuery(query);
   const [searchTerm, setSearchTerm] = useState("");
+  const [selectedContinents, setSelectedContinents] = useState([]);
+  const [visibleCountries, setVisibleCountries] = useState([]);
+  const [visibleCount, setVisibleCount] = useState(9);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
-  const filteredCountries = data.countries.filter((country) => {
-    const searchTermLower = searchTerm.toLowerCase();
-    return (
-      country.name?.toLowerCase().startsWith(searchTermLower) ||
-      country.native?.toLowerCase().startsWith(searchTermLower)
+  useEffect(() => {
+    const filteredCountries = data?.countries?.filter((country) => {
+      const searchTermLower = searchTerm.toLowerCase();
+      const matchesSearchTerm =
+        country.name?.toLowerCase().startsWith(searchTermLower) ||
+        country.native?.toLowerCase().startsWith(searchTermLower);
+
+      const matchesContinents =
+        selectedContinents.length === 0 ||
+        selectedContinents.includes(country.continent.code);
+
+      return matchesSearchTerm && matchesContinents;
+    });
+
+    setVisibleCountries(filteredCountries?.slice(0, visibleCount));
+  }, [data, searchTerm, selectedContinents, visibleCount]);
+
+  const handleLoadMore = () => {
+    setVisibleCount((prevCount) => prevCount + 9);
+  };
+
+  const handleContinentChange = (e) => {
+    const continent = e.target.value;
+    setSelectedContinents((prevSelected) =>
+      prevSelected.includes(continent)
+        ? prevSelected.filter((c) => c !== continent)
+        : [...prevSelected, continent]
     );
-  });
+    setVisibleCount(9);
+  };
 
   return (
     <div>
-      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <div className="flex items-center mb-4">
+        <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <div className="ml-4 relative">
+          <button
+            onClick={() => setDropdownOpen(!dropdownOpen)}
+            className="border py-2 px-4 rounded-md w-full max-w-xs"
+          >
+            Filtrar
+          </button>
+          {dropdownOpen && (
+            <div className="absolute mt-2 bg-white border rounded-md shadow-lg z-10">
+              {data?.continents?.map((continent) => (
+                <label key={continent.code} className="block px-4 py-2">
+                  <input
+                    type="checkbox"
+                    value={continent.code}
+                    checked={selectedContinents.includes(continent.code)}
+                    onChange={handleContinentChange}
+                    className="mr-2"
+                  />
+                  {continent.name}
+                </label>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {filteredCountries.map((country) => (
+        {visibleCountries.map((country) => (
           <CountryCard key={country.code} country={country} />
         ))}
       </div>
+      {visibleCount <
+        data?.countries?.filter((country) => {
+          const searchTermLower = searchTerm.toLowerCase();
+          const matchesSearchTerm =
+            country.name?.toLowerCase().startsWith(searchTermLower) ||
+            country.native?.toLowerCase().startsWith(searchTermLower);
+
+          const matchesContinents =
+            selectedContinents.length === 0 ||
+            selectedContinents.includes(country.continent.code);
+
+          return matchesSearchTerm && matchesContinents;
+        }).length && (
+        <button className="mt-4 p-2 border rounded" onClick={handleLoadMore}>
+          Ver más
+        </button>
+      )}
     </div>
   );
 }
--- src/components/country-card.js
+++ src/components/country-card.js
@@ -1,15 +1,77 @@
-const CountryCard = ({ country }) => {
-    const { name, capital, population, languages, currencies } = country;
+"use client";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 
-    return (
-        <div className="border p-4 rounded-md">
-            <h2 className="text-lg font-bold">{name}</h2>
-            <p><strong>Capital:</strong> {capital}</p>
-            <p><strong>Population:</strong> {population}</p>
-            <p><strong>Idioma:</strong> {languages.map(lang => lang.name).join(', ')}</p>
-            <p><strong>Currency:</strong> {currencies.map(curr => curr).join(', ')}</p>
-        </div>
-    );
+const UNSPLASH_ACCESS_KEY = "DkiSn52NtYDu5S9gWLIzE4OuRlz673kKcpurWlEg8Tc";
+
+const defaultImageUrl = "/public/images/not_found.jpg";
+
+const CountryCard = ({ country }) => {
+  const { name, capital, languages, currencies } = country;
+  const [image, setImage] = useState(null);
+  const [error, setError] = useState(null);
+  const { ref, inView } = useInView();
+  const [imageCache, setImageCache] = useState({});
+
+  useEffect(() => {
+    if (inView && !image && !error) {
+      const fetchImage = async () => {
+        try {
+          if (imageCache[name]) {
+            setImage(imageCache[name]);
+          } else {
+            const response = await fetch(
+              `https://api.unsplash.com/search/photos?query=${name}&client_id=${UNSPLASH_ACCESS_KEY}`
+            );
+            const data = await response.json();
+            if (data.results && data.results.length > 0) {
+              const imageUrl = data.results[0].urls.small;
+              setImageCache((prevCache) => ({
+                ...prevCache,
+                [name]: imageUrl,
+              }));
+              setImage(imageUrl);
+            } else {
+              setError(
+                "No se encontró ninguna imagen para este país en Unsplash."
+              );
+              setImage(defaultImageUrl);
+            }
+          }
+        } catch (error) {
+          setError("Hubo un error al cargar la imagen.");
+          setImage(defaultImageUrl);
+        }
+      };
+
+      fetchImage();
+    }
+  }, [name, inView, image, error, imageCache]);
+
+  return (
+    <div ref={ref} className="border p-4 rounded-md">
+      {image && (
+        <Image
+          src={image}
+          alt={`${name}`}
+          width={300}
+          height={200}
+          className="w-full h-32 object-cover rounded-md mb-4"
+        />
+      )}
+      <h2 className="text-lg font-bold">{name}</h2>
+      <p>
+        <strong>Capital:</strong> {capital}
+      </p>
+      <p>
+        <strong>Idioma:</strong> {languages.map((lang) => lang.name).join(", ")}
+      </p>
+      <p>
+        <strong>Moneda:</strong> {currencies.join(", ")}
+      </p>
+    </div>
+  );
 };
 
 export default CountryCard;
